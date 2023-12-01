30c4a248ea28f8de503a722814a833ab3bbea376
debbie-zj <dzha915@aucklanduni.ac.nz>
2023-12-01 18:19:04 +1300

    Tidy up plotting scripts

--- bivme/plotting/Plot_GuidePoints_html.py
+++ bivme/plotting/Plot_GuidePoints_html.py
@@ -1,8 +1,3 @@
-# Created by LDT on 16 Aug 2022
-# This script contains functions to plot guide points to html 
-
-
-#!/usr/bin/env python3
 import os
 import numpy as np
 import time
@@ -12,9 +7,12 @@ from plotly.offline import  plot
 import plotly.graph_objs as go
 import sys
 
-sys.path.append( '../BiV_Modelling_v2' ) # append path to the Fitting framework where the BiVFitting folder is located
+from bivme.fitting.GPDataSet import *
 
-from BiVFitting import *
+
+case_folder='R:\\resmed201900006-biomechanics-in-heart-disease\\Sandboxes\\Debbie\\collaborations\\chicago-rv-mesh\\analysis\\gpfiles-raw\\RV01' # this is the folder where I have stored your GP and SliceInfoFile
+
+suffix = ''
 
 
 def Plot_html(folder, **kwargs):
@@ -36,33 +34,36 @@ def Plot_html(folder, **kwargs):
     print('case', case)
 
     frameID = 'ED'
-    GPfilename = os.path.join(folder, 'GP_'+frameID+'.txt') 
-    filenameInfo = os.path.join(folder, 'SliceInfo.txt')
+    frameID = 'def'  
+    
+    GPfilename = os.path.join(folder, f'GPfile{suffix}.txt') 
+    filenameInfo = os.path.join(folder, 'SliceInfoFile.txt')
 
     # this section is only used to measure the shift
-    time_frame_ED = 1 # choose the correct time frame number
-    ED_dataset = GPDataSet(os.path.join(folder, 'GP_ED.txt'),filenameInfo, case, sampling = 1, time_frame_number = time_frame_ED)
+    time_frame_ED = 0 # choose the correct time frame number
+    # ED_dataset = GPDataSet(os.path.join(folder, 'GP_ED.txt'),filenameInfo, case, sampling = 1, time_frame_number = time_frame_ED)
+    ED_dataset = GPDataSet(GPfilename,filenameInfo, case, sampling = 1, time_frame_number = time_frame_ED)
     result_ED = ED_dataset.sinclaire_slice_shifting( frame_num = time_frame_ED) 
     shift_ED = result_ED[0]
     pos_ED = result_ED[1]   
             
-    contours_to_plot = [ContourType.LAX_RA,
+    contours_to_plot = [
                                 ContourType.SAX_RV_FREEWALL, ContourType.LAX_RV_FREEWALL,
                                 ContourType.SAX_RV_SEPTUM, ContourType.LAX_RV_SEPTUM,
-                                ContourType.SAX_LV_ENDOCARDIAL,
+                                ContourType.SAX_LV_ENDOCARDIAL, ContourType.SAX_RV_ENDOCARDIAL,
                                 ContourType.SAX_LV_EPICARDIAL, ContourType.RV_INSERT,
                                 ContourType.APEX_POINT, ContourType.MITRAL_VALVE,
                                 ContourType.TRICUSPID_VALVE,
-                                ContourType.SAX_RV_EPICARDIAL, ContourType.LAX_RV_EPICARDIAL,
+                                ContourType.SAX_RV_EPICARDIAL, ContourType.LAX_RV_EPICARDIAL, ContourType.LAX_RV_ENDOCARDIAL, 
                                 ContourType.LAX_LV_ENDOCARDIAL, ContourType.LAX_LV_EPICARDIAL,
-                                ContourType.LAX_RV_EPICARDIAL, ContourType.SAX_RV_OUTLET,
+                                ContourType.SAX_RV_OUTLET,
                                 ContourType.PULMONARY_PHANTOM, ContourType.AORTA_VALVE,
                                 ContourType.PULMONARY_VALVE, ContourType.TRICUSPID_PHANTOM,
-                                ContourType.AORTA_PHANTOM, ContourType.MITRAL_PHANTOM
+                                ContourType.AORTA_PHANTOM, ContourType.MITRAL_PHANTOM, ContourType.LAX_LV_EXTENT,ContourType.LAX_RV_EXTENT
                                 ]
 
     # load GP for chosen frame and apply shift measured at ED
-    time_frame_GP = 1
+    time_frame_GP = 0
     data_set = GPDataSet(GPfilename,filenameInfo, case, 1, time_frame_number = time_frame_GP) #18 is ES (RV) (timeframe 0-49)
     data_set.apply_slice_shift(shift_ED, pos_ED)
     contourPlots = data_set.PlotDataSet(contours_to_plot)
@@ -71,19 +72,14 @@ def Plot_html(folder, **kwargs):
 
     # plot hmtl time series for all chosen frames
     plot(go.Figure(contourPlots),filename=os.path.join(folder,
-                                           'GuidePoints_'+frameID+'.html'), auto_open=False)
+                                           f'GuidePoints_{suffix}_{time_frame_GP}.html'), auto_open=False)
                 
-
-
 
 if __name__ == '__main__':
 
     
     startLDT = time.time()
-
-    main_path = 'C:/Users/ldt18/Desktop/Dev_HPC'       
-    cases_folder = './venus1' # path where patient folders containing .txt models are located
-    results = Plot_html('./venus1')
-
+   
+    # results = Plot_html('./venus1')
+    results = Plot_html(case_folder)
     print('TOTAL TIME: ', time.time()-startLDT)
-
