838c07c927b2b74322af4f669275648aadfe61ea
Charlène Mauger <91033137+charlenemauger1@users.noreply.github.com>
2024-09-18 17:02:39 +0100

    move to postprocessing

--- src/bivme/postprocessing/detect_intersection.py
+++ src/bivme/postprocessing/detect_intersection.py
@@ -6,6 +6,7 @@ import os, sys
 from pathlib import Path
 import re
 import fnmatch
+from copy import deepcopy
 
 from bivme import MODEL_RESOURCE_DIR
 from bivme.meshing.mesh import Mesh
@@ -14,11 +15,27 @@ from loguru import logger
 from rich.progress import Progress
 import tomli
 import shutil
+from bivme.fitting.surface_enum import Surface
+from bivme.fitting.surface_enum import ContourType
+from bivme.fitting.GPDataSet import GPDataSet
 
 biv_model_folder = MODEL_RESOURCE_DIR
 
-#import pymeshfix
+# mapping 
+contour_map = {
+   Surface.RV_FREEWALL : ContourType.SAX_RV_FREEWALL,
+   Surface.RV_SEPTUM : ContourType.SAX_RV_SEPTUM,
+   Surface.LV_ENDOCARDIAL : ContourType.SAX_LV_ENDOCARDIAL,
+   Surface.RV_INSERT : ContourType.RV_INSERT,
+   Surface.APEX : ContourType.APEX_POINT,
+   Surface.MITRAL_VALVE : ContourType.MITRAL_VALVE,
+   Surface.TRICUSPID_VALVE : ContourType.TRICUSPID_VALVE,
+   Surface.AORTA_VALVE : ContourType.AORTA_VALVE,
+   Surface.PULMONARY_VALVE : ContourType.PULMONARY_VALVE}
+   # : ContourType.SAX_RV_EPICARDIAL
+   # : ContourType.SAX_LV_EPICARDIAL
 
+#import pymeshfix
 
 def fix_intersection(case_name: str, config: dict, model_file: os.PathLike, output_folder: os.PathLike, biv_model_folder: os.PathLike = MODEL_RESOURCE_DIR) -> None:
     """
@@ -37,20 +54,35 @@ def fix_intersection(case_name: str, config: dict, model_file: os.PathLike, outp
     current_collision = biventricular_model.detect_collision()
     inter = current_collision.difference(biventricular_model.reference_collision) 
 
-    print()
-    assert False
-
     if bool(inter):
+
         
-        my_logger.info(f"Intersections detected for case {os.path.basename(os.path.normpath(model_file))}")
+        logger.info(f"Intersections detected for case {os.path.basename(os.path.normpath(model_file))}")
         
         # initialise GP dataset from fitted model
 
         gp_dataset = GPDataSet()
 
-        biventricular_model = BiventricularModel(biv_model_folder, collision_detection = True)
+        points = []
+        slices = []
+        contour_types = []
+        weights = []
+        for surface, contours in contour_map.items():
+            start, end = biventricular_model.get_surface_vertex_start_end_index(surface)
 
-        my_logger.info(f"Refitting of {str(case)}")
+            #for idx in range(start, end+1)
+            
+
+            points.append([biventricular_model.et_pos[idx,:] for idx in range(start, end+1)])
+            slices.append([0 for idx in range(start, end+1)])
+            contour_types.append([contours for idx in range(start, end+1)])
+            weights.append([1.0 for idx in range(start, end+1)])
+
+            print(points)
+            assert False
+        #biventricular_model = BiventricularModel(biv_model_folder, collision_detection = True)
+
+        logger.info(f"Refitting of {str(case)}")
 
         residuals = 0
 
@@ -68,33 +100,33 @@ def fix_intersection(case_name: str, config: dict, model_file: os.PathLike, outp
         biventricular_model.update_pose_and_scale(data_set)
 
         if not data_set.success:
-            my_logger.error(f"Cannot initialize GPDataSet! Skipping this frame")
+            logger.error(f"Cannot initialize GPDataSet! Skipping this frame")
             return
 
         try:
             _ = data_set.create_valve_phantom_points(config["gp_processing"]["num_of_phantom_points_mv"], ContourType.MITRAL_VALVE)
         except:
-            my_logger.warning('Error in creating mitral phantom points')
+            logger.warning('Error in creating mitral phantom points')
 
         try:
             _ = data_set.create_valve_phantom_points(config["gp_processing"]["num_of_phantom_points_tv"], ContourType.TRICUSPID_VALVE)
         except:
-            my_logger.warning('Error in creating tricuspid phantom points')
+            logger.warning('Error in creating tricuspid phantom points')
 
         try:
             _ = data_set.create_valve_phantom_points(config["gp_processing"]["num_of_phantom_points_pv"], ContourType.PULMONARY_VALVE)
         except:
-            my_logger.warning('Error in creating pulmonary phantom points')
+            logger.warning('Error in creating pulmonary phantom points')
 
         try:
             _ = data_set.create_valve_phantom_points(config["gp_processing"]["num_of_phantom_points_av"], ContourType.AORTA_VALVE)
         except:
-            my_logger.warning('Error in creating aorta phantom points')
+            logger.warning('Error in creating aorta phantom points')
 
         contour_plots = data_set.plot_dataset(contours_to_plot)
 
         # Perform linear fit
-        solve_least_squares_problem(biventricular_model, config["fitting_weights"]["guide_points"], data_set, my_logger)
+        solve_least_squares_problem(biventricular_model, config["fitting_weights"]["guide_points"], data_set, logger)
 #
         ## Perform diffeomorphic fit
         residuals += solve_convex_problem(
@@ -103,7 +135,7 @@ def fix_intersection(case_name: str, config: dict, model_file: os.PathLike, outp
             config["fitting_weights"]["guide_points"],
             config["fitting_weights"]["convex_problem"],
             config["fitting_weights"]["transmural"],
-            my_logger,
+            logger,
         ) / len(sorted(frames_to_fit))
 
         # Plot final results
@@ -207,7 +239,7 @@ def fix_intersection(case_name: str, config: dict, model_file: os.PathLike, outp
                     output_folder_vtk, f"{case}_{key}_{num:03}.vtk"
                 )
                 write_vtk_surface(str(mesh_path), vertices, faces_mapped)
-                my_logger.success(f"{case}_{key}_{num:03}.vtk successfully saved to {output_folder_vtk}")
+                logger.success(f"{case}_{key}_{num:03}.vtk successfully saved to {output_folder_vtk}")
 
             elif output_format == ".obj":
                 output_folder_obj = Path(output_folder, f"obj{gp_suffix}")
@@ -216,9 +248,9 @@ def fix_intersection(case_name: str, config: dict, model_file: os.PathLike, outp
                     output_folder_obj, f"{case}_{key}_{num:03}.obj"
                 )
                 export_to_obj(mesh_path, vertices, faces_mapped)
-                my_logger.success(f"{case}_{key}_{num:03}.obj successfully saved to {output_folder_obj}")
+                logger.success(f"{case}_{key}_{num:03}.obj successfully saved to {output_folder_obj}")
             else:
-                my_logger.error('argument format must be .obj or .vtk')
+                logger.error('argument format must be .obj or .vtk')
                 return -1
 
         ##TODO remove duplicated code here - not sure how yet
@@ -247,7 +279,7 @@ def fix_intersection(case_name: str, config: dict, model_file: os.PathLike, outp
                         output_folder_vtk, f"{case}_{key}_{num:03}_control_mesh.vtk"
                     )
                     write_vtk_surface(str(mesh_path), vertices, faces_mapped)
-                    my_logger.success(f"{case}_{key}_{num:03}_control_mesh.vtk successfully saved to {output_folder_vtk}")
+                    logger.success(f"{case}_{key}_{num:03}_control_mesh.vtk successfully saved to {output_folder_vtk}")
 
                 elif output_format == ".obj":
                     output_folder_obj = Path(output_folder, f"obj{gp_suffix}")
@@ -256,18 +288,17 @@ def fix_intersection(case_name: str, config: dict, model_file: os.PathLike, outp
                         output_folder_obj, f"{case}_{key}_{num:03}_control_mesh.obj"
                     )
                     export_to_obj(mesh_path, vertices, faces_mapped)
-                    my_logger.success(f"{case}_{key}_{num:03}_control_mesh.obj successfully saved to {output_folder_obj}")
+                    logger.success(f"{case}_{key}_{num:03}_control_mesh.obj successfully saved to {output_folder_obj}")
                 else:
-                    my_logger.error('argument format must be .obj or .vtk')
+                    logger.error('argument format must be .obj or .vtk')
                     return -1
 
         return residuals
         print(model_file)
 
     else:
-        my_logger.info("No intersection detected - moving on")
+        logger.success(f"No intersection detected for {case_name} - moving on")
         return
-    #print("inter", bool(inter))
 
 
 if __name__ == "__main__":
@@ -323,17 +354,20 @@ if __name__ == "__main__":
     output_folder.mkdir(parents=True, exist_ok=True)
     shutil.copy(args.config_file, output_folder)
 
-    folders = [p.name for p in Path(config["output"]["output_directory"]).glob(args.patterns) if os.path.isdir(p)]
+
+    case_list = os.listdir(config["output"]["output_directory"])
+    folders = [Path(config["output"]["output_directory"], case).as_posix() for case in case_list]
 
     logger.info(f"Found {len(folders)} model folders.")
 
     try:
         for i, folder in enumerate(folders):
+
             rule = re.compile(fnmatch.translate("*model_frame*.txt"), re.IGNORECASE)
-            models = [config["output"]["output_directory"] / folder / Path(name) for name in os.listdir(config["output"]["output_directory"] / folder) if rule.match(name)]
+            models = [folder / Path(name) for name in os.listdir(Path(folder)) if rule.match(name)]
             models = sorted(models)
 
-            logger.info(f"Processing {str(args.model_dir / folder)} ({i+1}/{len(folders)})")
+            logger.info(f"Processing {str(folder)} ({i+1}/{len(folders)})")
             with Progress(transient=True) as progress:
                 task = progress.add_task("Checking for intersection...", total=len(models))
                 console = progress
@@ -341,7 +375,6 @@ if __name__ == "__main__":
                 for biv_model_file in models:
                     fix_intersection(folder, config, biv_model_file, output_folder, biv_resource_folder)
                     progress.advance(task)
-                    #assert False
 
         logger.success(f"Done. Results are saved in {output_folder}")
     except KeyboardInterrupt:
