56d6ac530412db3e059dbf40b4cacc7b9af2beb4
Charlene <charlene.mauger1@gmail.com>
2024-11-28 13:56:10 +0000

    add badge and modify trhu wall in test

--- src/bivme/analysis/compute_wall_thickness.py
+++ src/bivme/analysis/compute_wall_thickness.py
@@ -47,7 +47,7 @@ def find_wall_thickness(case_name: str, model_file: os.PathLike, output_folder: 
     assert material_file.exists(), \
         f"biv_model_folder does not exist. Cannot find {material_file} file!"
 
-    thru_wall_file = biv_model_folder / 'epi_to_septum_ETindices.txt'
+    thru_wall_file = biv_model_folder / 'thru_wall_et_indices.txt'
     assert thru_wall_file.exists(), \
         f"biv_model_folder does not exist. Cannot find {thru_wall_file} file!"
 
