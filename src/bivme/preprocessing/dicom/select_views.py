11456210374da3027be2e5f107d857b8e1e3cbb1
Joshua Dillon <jdil469@aucklanduni.ac.nz>
2025-03-28 14:40:53 +1300

    Implement logic for handling conflicting predictions

--- src/bivme/preprocessing/dicom/select_views.py
+++ src/bivme/preprocessing/dicom/select_views.py
@@ -29,9 +29,9 @@ def select_views(patient, src, dst, model, states, option, my_logger):
         metadata_view_predictions = pd.read_csv(metadata_csv_path)
         image_view_predictions = pd.read_csv(image_csv_path)
 
-        all_series = set(metadata_view_predictions['Series Number'].values + image_view_predictions['Series Number'].values)
+        all_series = list(set(np.concatenate([metadata_view_predictions['Series Number'].values,image_view_predictions['Series Number'].values])))
         view_predictions_array = []
-        refinement_map = {'2ch': 'LAX', '3ch': 'LAX', '4ch': 'LAX', 'RVOT': 'RVOT', 'RVOT-T': 'RVOT', '2ch-RT': 'LAX', 'LVOT': 'LVOT', 'SAX': 'SAX', 'SAX-atria': 'SAX', 'OTHER': 'SAX'}
+        refinement_map = {'2ch': 'LAX', '2ch-RT': 'LAX', '3ch': 'LAX', '4ch': 'LAX', 'LVOT': 'LVOT', 'OTHER': 'SAX', 'RVOT': 'RVOT', 'RVOT-T': 'RVOT', 'SAX': 'SAX', 'SAX-atria': 'SAX'} # Map refined views to broad views (SAX, LAX, RVOT, LVOT)
         for series in all_series:
             metadata_row = metadata_view_predictions[metadata_view_predictions['Series Number'] == series]
             image_row = image_view_predictions[image_view_predictions['Series Number'] == series]
@@ -39,12 +39,28 @@ def select_views(patient, src, dst, model, states, option, my_logger):
             metadata_broad_pred = refinement_map[metadata_row['Predicted View'].values[0]]
             image_broad_pred = refinement_map[image_row['Predicted View'].values[0]]
 
-            if metadata_broad_pred == image_broad_pred:
+            if metadata_broad_pred == image_broad_pred: # If metadata and image-based predictions agree on broad view, use the image-based prediction
                 view_predictions_array.append([series, image_row['Predicted View'].values[0], image_row['Vote Share'].values[0], image_row['Frames Per Slice'].values[0]])
-            else:
-                pass # TODO: Implement logic for when metadata and image-based predictions differ
 
+            else: # If metadata and image-based predictions conflict, use the metadata-based prediction to adjust the image-based prediction
+                my_logger.info(f'Series {series} metadata and image-based predictions conflict: {metadata_broad_pred} vs {image_broad_pred}. Using metadata-based prediction to correct image-based prediction...') # TODO: Remove after debugging
+                # Zero out the confidence of the incorrect categories
+                confidences = []
+                for v in list(refinement_map.keys()):
+                    if refinement_map[v] != metadata_broad_pred:
+                        image_row[f'{v} confidence'] = 0
+                    confidences.append(image_row[f'{v} confidence'].values[0])
+                
+                # Image prediction is the view with the highest confidence remaining
+                image_pred = list(refinement_map.keys())[np.argmax(confidences)]
+                view_predictions_array.append([series, image_pred, 0, image_row['Frames Per Slice'].values[0]])
+                
         view_predictions = pd.DataFrame(view_predictions_array, columns=['Series Number', 'Predicted View', 'Vote Share', 'Frames Per Slice'])
+        csv_path = os.path.join(dst, 'view-classification', 'view_predictions.csv')
+        viewSelector.csv_path = csv_path
+        view_predictions.to_csv(csv_path, mode='w', index=False)
+        os.remove(metadata_csv_path)
+        os.remove(image_csv_path)
 
         ## Flag any slices with non-matching number of phases
         # Use the SAX series as the reference for the 'right' number of phases
@@ -80,40 +96,40 @@ def select_views(patient, src, dst, model, states, option, my_logger):
         else:
             repeated_series = viewSelector.df.iloc[idx]
             repeated_series_num = repeated_series['Series Number'].values
-            # Order by series number, so that if that if two series have the same confidence, the higher series number is retained
+            # Order by series number, so that if that if two series have the same vote share, the higher series number is retained
             repeated_series_num = sorted(repeated_series_num, reverse=True)
             repeated_series_num = np.array(repeated_series_num)
 
-            # Retain only the series with the highest confidence, convert the rest to 'Excluded'
-            confidences = [view_predictions[view_predictions['Series Number'] == x]['Confidence'].values[0] for x in repeated_series_num]
+            # Retain only the series with the highest vote share, convert the rest to 'Excluded'
+            vote_shares = [view_predictions[view_predictions['Series Number'] == x]['Vote Share'].values[0] for x in repeated_series_num]
 
-            idx_max = np.argmax(confidences)
+            idx_max = np.argmax(vote_shares)
             idx_to_exclude = [i for i in range(len(repeated_series_num)) if i != idx_max]
 
             view_predictions.loc[view_predictions['Series Number'].isin(repeated_series_num[idx_to_exclude]), 'Predicted View'] = 'Excluded'
 
-            my_logger.info(f'Excluded series {repeated_series_num[idx_to_exclude]} due to duplicate slice location.')
+            my_logger.info(f'Excluded series {repeated_series_num[idx_to_exclude]} as they exist at the same slice location as another series.')
     
-        # Type 2 - Multiple series classed as the same 'exclusive' view (i.e. 2ch, 3ch, 4ch, RVOT, RVOT-T 2ch-RT, RVOT-T, LVOT) 
+        # Type 2 - Multiple series classed as the same 'exclusive' view (i.e. 2ch, 3ch, 4ch, RVOT, RVOT-T, 2ch-RT, RVOT-T, LVOT) 
         # i.e. a view that should only have one series 
         exclusive_views = ['2ch', '3ch', '4ch', 'RVOT', 'RVOT-T', '2ch-RT', 'LVOT']
         for view in exclusive_views:
             series = view_predictions[view_predictions['Predicted View'] == view]
             series_nums = series['Series Number'].values
-            # Order by series number, so that if that if two series have the same confidence, the higher series number is retained
+            # Order by series number, so that if that if two series have the same vote share, the higher series number is retained
             series_nums = sorted(series_nums, reverse=True)
             series_nums = np.array(series_nums)
 
             if len(series) > 1:
                 my_logger.info(f'Multiple series classed as {view}.')
 
-                confidences = [view_predictions[view_predictions['Series Number'] == x]['Confidence'].values[0] for x in series_nums]
+                vote_shares = [view_predictions[view_predictions['Series Number'] == x]['Vote Share'].values[0] for x in series_nums]
 
-                idx_max = np.argmax(confidences)
+                idx_max = np.argmax(vote_shares)
                 idx_to_exclude = [i for i in range(len(series)) if i != idx_max]
                 view_predictions.loc[view_predictions['Series Number'].isin(series_nums[idx_to_exclude]), 'Predicted View'] = 'Excluded'
 
-                my_logger.info(f'Excluded series {series_nums[idx_to_exclude]} due to multiple series classed as {view}.')
+                my_logger.info(f'Excluded series {series_nums[idx_to_exclude]} as multiple series are classed as {view}.')
 
         # Print summary to log
         my_logger.info(f'View predictions for {patient}:')
