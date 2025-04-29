66b74821b6a6071364351b17e6853c8ad3c81f93
Joshua Dillon <jdil469@aucklanduni.ac.nz>
2025-04-29 21:09:18 +1200

    Rework to complete (pp+fitting) one case at a time

--- src/bivme/preprocessing/dicom/export_guidepoints.py
+++ src/bivme/preprocessing/dicom/export_guidepoints.py
@@ -3,18 +3,10 @@ import shutil
 
 from bivme.preprocessing.dicom.src.sliceviewer import SliceViewer
 
-def export_guidepoints(case, case_dst, output_folder, slice_dict, slice_mapping):
-    output_folder = os.path.join(output_folder, case)
-
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    else:
-        shutil.rmtree(output_folder)
-        os.makedirs(output_folder)
-
+def export_guidepoints(dst, output, slice_dict, slice_mapping):
     for s in slice_dict.values():
-        s.export_slice(output_folder, slice_mapping)
+        s.export_slice(output, slice_mapping)
 
     # Move slice info file to output folder
-    shutil.copyfile(os.path.join(case_dst, 'SliceInfoFile.txt'), os.path.join(output_folder, 'SliceInfoFile.txt'))
+    shutil.copyfile(os.path.join(dst, 'SliceInfoFile.txt'), os.path.join(output, 'SliceInfoFile.txt'))
     
